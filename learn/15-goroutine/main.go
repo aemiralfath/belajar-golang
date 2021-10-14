f972626b34fd7dfe4b2ecbec3c94f16992c2666f
criptdestroyer <ahmademir1201@gmail.com>
2021-10-14 14:27:18 +0700

    add goroutine and channel exercise

--- learn/15-goroutine/main.go
+++ learn/15-goroutine/main.go
@@ -1,9 +1,33 @@
 package main
 
-func main(){
-	// 3 task concurrent
-	// pertask beda2 sleepnya
-	// gunakan wait group dan channel
-	// waitnya tidak menunggu dari 5 detik
-	// cancelation bisa menggunakn time, context, channel
-}	
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	var messages = make(chan string)
+
+	go print(5, "concurrent", messages)
+	go print(10, "sequential", messages)
+	go print(15, "parallel", messages)
+
+	var message1 = <-messages
+	fmt.Println(message1)
+
+	var message2 = <-messages
+	fmt.Println(message2)
+
+	var message3 = <-messages
+	fmt.Println(message3)
+}
+
+func print(till int, message string, messages chan string) {
+	result := ""
+	for i := 0; i < till; i++ {
+		result += fmt.Sprintf("hello %d %s \n", (i + 1), message)
+	}
+	messages <- result
+}
